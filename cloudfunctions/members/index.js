c444d29d643a0c68ae955fd3440631c9077f621d
lintao <lintaotoys.com>
2026-03-30 13:12:42 +0800

    增加会员注册登陆页面，因为相关权限问题，暂时停止

--- cloudfunctions/members/index.js
+++ cloudfunctions/members/index.js
@@ -7,16 +7,27 @@ const collection = db.collection('members')
 
 exports.main = async (event, context) => {
   const { action, data, page = 1, pageSize = 10, keyword } = event
+  const wxContext = cloud.getWXContext()
+  const openid = wxContext.OPENID
 
   switch (action) {
-    case 'add':
-      // 新增会员
+    case 'add': {
+      // 新增会员，openid 唯一
+      const exist = await collection.where({ openid }).get()
+      if (exist.data && exist.data.length > 0) {
+        return { errMsg: 'already registered', data: exist.data[0] }
+      }
+      data.openid = openid
       data.registerTime = db.serverDate()
       data.winRate = 0
       data.score = 0
       return await collection.add({ data })
-    case 'search':
-      // 查询会员，支持姓名/手机号模糊，分页
+    }
+    case 'search': {
+      // 查询会员，支持 openid 精确查找，或姓名/手机号模糊，分页
+      if (event.byOpenid) {
+        return await collection.where({ openid }).get()
+      }
       const query = []
       if (keyword) {
         query.push(
@@ -31,6 +42,7 @@ exports.main = async (event, context) => {
         .skip((page - 1) * pageSize)
         .limit(pageSize)
         .get()
+    }
     case 'update':
       // 修改会员
       return await collection.doc(data._id).update({ data })
--- miniprogram/pages/mine/index.js
+++ miniprogram/pages/mine/index.js
@@ -1,1 +1,79 @@
-Page({})
+Page({
+	data: {
+		isLogin: false,
+		userInfo: {
+			avatarUrl: '',
+			name: '',
+			level: 0,
+			winRate: 0,
+			score: 0,
+			status: '',
+			intro: ''
+		}
+	},
+	onLoad() {
+		// 这里可以根据实际业务判断是否已登录，并拉取用户信息
+		// 示例：this.getUserInfo()
+	},
+	onLogin() {
+    console.log('onl;ognge')
+		wx.getUserProfile({
+			desc: '用于完善会员资料',
+			success: res => {
+				const wxUser = res.userInfo;
+				// 用 openid 精确查找会员
+				wx.cloud.callFunction({
+					name: 'members',
+					data: {
+						action: 'search',
+						byOpenid: true
+					},
+					success: searchRes => {
+						if (searchRes.result && searchRes.result.data && searchRes.result.data.length > 0) {
+							// 已注册，展示会员信息
+							const member = searchRes.result.data[0];
+							this.setData({
+								isLogin: true,
+								userInfo: {
+									avatarUrl: member.avatarUrl || wxUser.avatarUrl,
+									name: member.name,
+									level: member.level || 0,
+									winRate: member.winRate || 0,
+									score: member.score || 0,
+									status: member.status || '',
+									intro: member.intro || ''
+								}
+							})
+						} else {
+							// 未注册，注册会员
+							wx.cloud.callFunction({
+								name: 'members',
+								data: {
+									action: 'add',
+									data: {
+										name: wxUser.nickName,
+										avatarUrl: wxUser.avatarUrl
+									}
+								},
+								success: addRes => {
+									this.setData({
+										isLogin: true,
+										userInfo: {
+											avatarUrl: wxUser.avatarUrl,
+											name: wxUser.nickName,
+											level: 0,
+											winRate: 0,
+											score: 0,
+											status: '',
+											intro: ''
+										}
+									})
+								}
+							})
+						}
+					}
+				})
+			}
+		})
+	}
+})
