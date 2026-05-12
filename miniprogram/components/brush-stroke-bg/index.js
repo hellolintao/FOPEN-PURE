6c2681dedfc5db5f6184c13e955250411a894dff
xiaole.liao <xiaole.liao@zettlab.com>
2026-05-12 21:35:44 +0800

    feat(components): brush-stroke-bg 装饰元素

---
+++ miniprogram/components/brush-stroke-bg/index.js
@@ -0,0 +1,4 @@
+const SVG = `<svg viewBox='0 0 200 100' xmlns='http://www.w3.org/2000/svg'><path d='M10,60 Q40,20 80,40 T180,50' stroke='%23BEE645' stroke-width='40' fill='none' stroke-linecap='round'/></svg>`;
+Component({
+  data: { svgEncoded: SVG }
+});
