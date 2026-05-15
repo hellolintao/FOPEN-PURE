250e7eba3ed49c8b1068ac692cb00eb0d7d0b192
xiaole.liao <xiaole.liao@zettlab.com>
2026-05-16 03:26:58 +0800

    feat(miniprogram): status-tag component for v2.1 unified state badges

---
+++ miniprogram/components/status-tag/index.js
@@ -0,0 +1,9 @@
+Component({
+  options: { multipleSlots: false },
+  properties: {
+    type: { type: String, value: 'muted' },
+    text: { type: String, value: '' },
+    count: { type: null, value: null },
+    size: { type: String, value: 'md' },
+  },
+})
