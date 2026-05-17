d7a1b0be6a9e6298cfb06da189e877a9c8dfce45
xiaole.liao <xiaole.liao@zettlab.com>
2026-05-17 18:31:16 +0800

    test(miniprogram): align play style constants test

--- miniprogram/utils/__tests__/play-style.test.js
+++ miniprogram/utils/__tests__/play-style.test.js
@@ -6,13 +6,14 @@ const {
 
 test('exports shared play style constants for miniprogram packaging', () => {
   expect(PLAY_STYLE_VALUES).toEqual([
-    'baseliner',
-    'serve-volleyer',
-    'all-court',
-    'counter-puncher',
-    'aggressive-baseliner'
+    'ice-cow',
+    'vers',
+    'iron-lady',
+    'moon-queen',
+    'grinder',
+    'slicer'
   ])
   expect(PLAY_STYLE_OPTIONS.map((option) => option.value)).toEqual(PLAY_STYLE_VALUES)
-  expect(getPlayStyleLabel('all-court')).toBe('全场型')
+  expect(getPlayStyleLabel('moon-queen')).toBe('月亮女王')
   expect(getPlayStyleLabel('unknown')).toBe('')
 })
